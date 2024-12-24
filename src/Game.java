50dc222c6ed33a34a93c8eb1efdda261c6a42b76
Constantin-Alin <clmalex99@gmail.com>
2024-12-24 10:56:56 +0100

    Add repetition

--- src/Game.java
+++ src/Game.java
@@ -4,15 +4,19 @@ public class Game {
     public static void main(String[] args) {
         int hiddenNumber = 97;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Guess the number: ");
-        int guessedNumber = sc.nextInt();
 
-        if (guessedNumber < hiddenNumber) {
-            System.out.println("More");
-        } else if (guessedNumber > hiddenNumber) {
-            System.out.println("Less");
-        } else {
-            System.out.println("You guessed!");
+        while (true) {
+            System.out.println("Guess the number: ");
+            int guessedNumber = sc.nextInt();
+
+            if (guessedNumber < hiddenNumber) {
+                System.out.println("More");
+            } else if (guessedNumber > hiddenNumber) {
+                System.out.println("Less");
+            } else {
+                System.out.println("You guessed!");
+                break;
+            }
         }
     }
 }
