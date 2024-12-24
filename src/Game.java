ef1851d93858c1132e084334131dba4037edb289
Constantin-Alin <clmalex99@gmail.com>
2024-12-24 11:08:51 +0100

    Add attempts counter

--- src/Game.java
+++ src/Game.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         Random rnd = new Random();
+        int counter = 0;
         int hiddenNumber = rnd.nextInt(100);
         Scanner sc = new Scanner(System.in);
 
         while (true) {
             System.out.println("Guess the number: ");
             int guessedNumber = sc.nextInt();
+            counter += 1;
 
             if (guessedNumber < hiddenNumber) {
                 System.out.println("More");
@@ -17,6 +19,7 @@ public class Game {
                 System.out.println("Less");
             } else {
                 System.out.println("You guessed!");
+                System.out.println("Attempts used: " + counter);
                 break;
             }
         }
